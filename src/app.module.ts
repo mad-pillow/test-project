b7e3a4aaf5ca88a35fe4920932730546d1be5eb6
Dmitriy Sychov <d.sychov@andersenlab.com>
2022-08-20 10:43:24 +0300

    PD-001: added basic logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import base_config from '../config/base_config';
+import { PortalDemandController } from './portalDemand/portalDemand.controller';
+import { PortalDemandService } from './portalDemand/portalDemand.service';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ load: [base_config] })],
+  controllers: [AppController, PortalDemandController],
+  providers: [AppService, PortalDemandService],
 })
 export class AppModule {}
